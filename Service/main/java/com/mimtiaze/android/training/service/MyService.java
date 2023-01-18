e311e0c037b8ea4f17be76d0f66b122a026f579a
Mohammad Maruf Imtiaze <mimtiaze@gmail.com>
2023-01-18 07:33:53 +0600

    Service modified, added toast

--- Service/main/java/com/mimtiaze/android/training/service/MyService.java
+++ Service/main/java/com/mimtiaze/android/training/service/MyService.java
@@ -4,10 +4,12 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MyService extends Service {
     public final static String TAG = "Service";
 
+    Toast mToast;
     public MyService() {
     }
 
@@ -21,12 +23,17 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate");
+        mToast = Toast.makeText(this, "Service created", Toast.LENGTH_SHORT);
+        mToast.show();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
+        mToast = Toast.makeText(this, "Service started", Toast.LENGTH_SHORT);
+        mToast.show();
         return super.onStartCommand(intent, flags, startId);
+
 
     }
 
@@ -34,5 +41,7 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+        mToast = Toast.makeText(this, "Service destroyed", Toast.LENGTH_SHORT);
+        mToast.show();
     }
 }
